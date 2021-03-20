76b4414e25d1df5428504a7d53b191c45654a941
余成 <81008814+CHENGYU0407@users.noreply.github.com>
2021-03-20 17:37:00 +0800

    上传文件

---
+++ Userdao.java
@@ -0,0 +1,11 @@
+package annotation.dao;
+
+import org.springframework.stereotype.Repository;
+
+//增强的类
+@Repository
+public class Userdao {
+public void add() {
+	System.out.println("add--");
+}
+}
---
+++ Userdao2.java
@@ -0,0 +1,10 @@
+package annotation.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Userdao2 {
+public void add(){
+	int i=1/0;
+}
+}
